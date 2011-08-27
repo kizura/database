92e7b9f4dbe5bb411cfbae84962117d0af31a9b5
gonzo1247 <gonzo1247@yahoo.de>
2011-08-28 01:43:02 +0200

    NPC Wuetender_Phanter angreifbar für die quest

---
+++ zp_world/Creature_template_Wuetender_Phanter_14_08_2011.sql
@@ -0,0 +1,3 @@
+UPDATE creature_template SET unit_flags = 64 WHERE entry = 10992;
+UPDATE creature_template SET dynamicflags = 0 WHERE entry = 10992;
+UPDATE creature_template SET family = 2 WHERE entry = 10992;
