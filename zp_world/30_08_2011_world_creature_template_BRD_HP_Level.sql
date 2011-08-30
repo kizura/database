377f3960f77b12624d357462809318e0ba9bac86
gonzo1247 <gonzo1247@yahoo.de>
2011-08-30 19:55:56 +0200

    boss level and HP adapted to old.wowhead.com.

---
+++ zp_world/30_08_2011_world_creature_template_BRD_HP_Level.sql
@@ -0,0 +1,11 @@
+UPDATE `creature_template` SET `minlevel` = '49', `maxlevel` = '52', `minhealth` = '23710', `maxhealth` = '23710' WHERE `entry` = '9016';
+UPDATE `creature_template` SET `minhealth` = '10670', `maxhealth` = '10670' WHERE `entry` = '10096';
+UPDATE `creature_template` SET `minlevel` = '51', `maxlevel` = '55', `minhealth` = '12546', `maxhealth` = '12546', `minmana` = '16936', `maxmana` = '16936' WHERE `entry` = '8983';
+UPDATE `creature_template` SET `minlevel` = '48', `maxlevel` = '53', `minhealth` = '15944', `maxhealth` = '15944' WHERE `entry` = '9017';
+UPDATE `creature_template` SET `minlevel` = '48', `maxlevel` = '53', `minhealth` = '27944', `maxhealth` = '27944' WHERE `entry` = '9056';
+UPDATE `creature_template` SET `minlevel` = '50', `maxlevel` = '54', `minhealth` = '8104', `maxhealth` = '9704', `minmana` = '10205', `maxmana` = '10205' WHERE `entry` = '9041';
+UPDATE `creature_template` SET `minlevel` = '49', `maxlevel` = '53', `minhealth` = '9812', `maxhealth` = '11412' WHERE `entry` = '9042';
+UPDATE `creature_template` SET `minlevel` = '50', `maxlevel` = '55', `minhealth` = '26140', `maxhealth` = '26140' WHERE `entry` = '9156';
+UPDATE `creature_template` SET `minlevel` = '53', `maxlevel` = '56', `minhealth` = '24291', `maxhealth` = '24291' WHERE `entry` = '9938';
+UPDATE `creature_template` SET `minlevel` = '52', `maxlevel` = '55', `minhealth` = '13592', `maxhealth` = '13592' WHERE `entry` = '8929';
+UPDATE `creature_template` SET `minlevel` = '53', `maxlevel` = '56', `minhealth` = '24291', `maxhealth` = '24291' WHERE `entry` = '9019';
