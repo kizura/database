5ebe8094f219fc62c49c29a8aa37d8cc171b8553
gonzo1247 <gonzo1247@yahoo.de>
2011-08-30 04:23:48 +0200

    Temp fix for Baer Gar quest item. Spell does not work.

---
+++ zp_world/30_08_2011_world_creature_loot_template_fire_essenz.sql
@@ -0,0 +1,2 @@
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`)
+VALUE ('9016', '11230', '-100', '0', '1', '1', '0', '0','0');
