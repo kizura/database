d063ba90a94cb3f50ef0eb1a0480e7ada5b68d96
gonzo1247 <gonzo1247@yahoo.de>
2011-09-25 01:50:10 +0200

    Fix HP and Mana for bosse and Trash in Dire Maul east/west/north
    Information from old.wowhead.com for HP/Mana

---
+++ zp_world/custom/25_09_2011_world_creature_template_dire_maul.sql
@@ -0,0 +1,62 @@
+UPDATE `creature_template` SET `minhealth` = '12954', `maxhealth` = '12954' WHERE `entry` = '13197';
+UPDATE `creature_template` SET `minhealth` = '6681', `maxhealth` = '6891' WHERE `entry` = '11461';
+UPDATE `creature_template` SET `minhealth` = '16704', `maxhealth` = '17226' WHERE `entry` = '13285';
+UPDATE `creature_template` SET `minhealth` = '15198', `maxhealth` = '15684' WHERE `entry` = '13196';
+UPDATE `creature_template` SET `minhealth` = '8097', `maxhealth` = '8352' WHERE `entry` = '11452';
+UPDATE `creature_template` SET `minhealth` = '6681', `maxhealth` = '6681', `minmana` = '3362', `maxmana` = '3362' WHERE `entry` = '11453';
+UPDATE `creature_template` SET `minhealth` = '418' , `maxhealth` = '418' WHERE `entry` = '14350';
+UPDATE `creature_template` SET `minhealth` = '8097', `maxhealth` = '8097' WHERE `entry` = '11456';
+UPDATE `creature_template` SET `minhealth` = '5490', `maxhealth` = '5667' WHERE `entry` = '11455';
+UPDATE `creature_template` SET `minhealth` = '7842', `maxhealth` = '8097' WHERE `entry` = '11454';
+UPDATE `creature_template` SET `minhealth` = '7842', `maxhealth` = '8097' WHERE `entry` = '11451';
+
+-- ---- dired maul west --- ----
+
+UPDATE `creature_template` SET `minhealth` = '6960', `maxhealth` = '7402' WHERE `entry` = '14303';
+UPDATE `creature_template` SET `minhealth` = '8352', `maxhealth` = '8613' WHERE `entry` = '11465';
+UPDATE `creature_template` SET `minhealth` = '6273', `maxhealth` = '6477', `minmana` = '3244', `maxmana` = '3244' WHERE `entry` = '11464';
+UPDATE `creature_template` SET `minhealth` = '16704', `maxhealth` = '17766' WHERE `entry` = '11459';
+UPDATE `creature_template` SET `minhealth` = '16194', `maxhealth` = '16194' WHERE `entry` = '13021';
+UPDATE `creature_template` SET `minhealth` = '7107', `maxhealth` = '7326', `minmana` = '9476', `maxmana` = '9736' WHERE `entry` = '11480';
+UPDATE `creature_template` SET `minhealth` = '474', `maxhealth` = '488', `minmana` = '4738', `maxmana` = '4868' WHERE `entry` = '14397';
+UPDATE `creature_template` SET `minhealth` = '6681', `maxhealth` = '7107', `minmana` = '9156', `maxmana` = '9476' WHERE `entry` = '11483';
+UPDATE `creature_template` SET `minhealth` = '33572', `maxhealth` = '33572' WHERE `entry` = '11489';
+UPDATE `creature_template` SET `minhealth` = '18312', `maxhealth` = '18312' WHERE `entry` = '14483';
+UPDATE `creature_template` SET `minhealth` = '31080', `maxhealth` = '31080' WHERE `entry` = '14506';
+UPDATE `creature_template` SET `minhealth` = '3968', `maxhealth` = '3968' WHERE `entry` = '14504';
+UPDATE `creature_template` SET `minhealth` = '2961', `maxhealth` = '3052' WHERE `entry` = '14482';
+UPDATE `creature_template` SET `minhealth` = '8613', `maxhealth` = '8883' WHERE `entry` = '11475';
+UPDATE `creature_template` SET `minhealth` = '6891', `maxhealth` = '7107', `minmana` = '6867', `maxmana` = '7107' WHERE `entry` = '11473';
+UPDATE `creature_template` SET `minhealth` = '629', `maxhealth` = '629' WHERE `entry` = '14396';
+UPDATE `creature_template` SET `minhealth` = '37728', `maxhealth` = '37728' WHERE `entry` = '11496';
+UPDATE `creature_template` SET `minhealth` = '31440', `maxhealth` = '31440' WHERE `entry` = '11486';
+UPDATE `creature_template` SET `minhealth` = '3919', `maxhealth` = '3732' WHERE `entry` = '11476';
+UPDATE `creature_template` SET `minhealth` = '3732', `maxhealth` = '3849' WHERE `entry` = '11477';
+UPDATE `creature_template` SET `minhealth` = '20757', `maxhealth` = '20757' WHERE `entry` = '11487';
+UPDATE `creature_template` SET `minhealth` = '17766', `maxhealth` = '17766' WHERE `entry` = '11467';
+UPDATE `creature_template` SET `minhealth` = '8352', `maxhealth` = '8613' WHERE `entry` = '11472';
+UPDATE `creature_template` SET `minhealth` = '6891', `maxhealth` = '6891', `minmana` = '6867', `maxmana` = '6867' WHERE `entry` = '11471';
+UPDATE `creature_template` SET `minhealth` = '6891', `maxhealth` = '7107' WHERE `entry` = '14398';
+UPDATE `creature_template` SET `minhealth` = '2159', `maxhealth` = '2297', `minmana` = '2163', `maxmana` = '2289', `faction_A` = '32', `faction_H` = '32', `npcflag` = '2' WHERE `entry` = '14364';
+UPDATE `creature_template` SET `minhealth` = '17094', `maxhealth` = '17094' WHERE `entry` = '11488';
+UPDATE `creature_template` SET `minhealth` = '15260', `maxhealth` = '15260' WHERE `entry` = '14308';
+
+-- ---- dire maul north ---- ----
+
+UPDATE `creature_template` SET `minhealth` = '14355', `maxhealth` = '14805' WHERE `entry` = '14351';
+UPDATE `creature_template` SET `minhealth` = '9745', `maxhealth` = '10050', `minmana` = '10680', `maxmana` = '10922' WHERE `entry` = '11444';
+UPDATE `creature_template` SET `minhealth` = '1', `maxhealth` = '1', `flags_extra` = '64' WHERE `entry` = '11446';
+UPDATE `creature_template` SET `minhealth` = '3758', `maxhealth` = '3997' WHERE `entry` = '13036';
+UPDATE `creature_template` SET `minhealth` = '23688', `maxhealth` = '23688' WHERE `entry` = '14326';
+UPDATE `creature_template` SET `minhealth` = '835', `maxhealth` = '861' WHERE `entry` = '13160';
+UPDATE `creature_template` SET `minhealth` = '10365', `maxhealth` = '10680', `minmana` = '16794', `maxmana` = '17253' WHERE `entry` = '11448';
+UPDATE `creature_template` SET `minhealth` = '3189', `maxhealth` = '3293' WHERE `entry` = '11442';
+UPDATE `creature_template` SET `minhealth` = '13920', `maxhealth` = '14355' WHERE `entry` = '11441';
+UPDATE `creature_template` SET `minhealth` = '14214', `maxhealth` = '14652', `minmana` = '4738', `maxmana` = '4868' WHERE `entry` = '11445';
+UPDATE `creature_template` SET `minhealth` = '23688', `maxhealth` = '23688' WHERE `entry` = '14321';
+UPDATE `creature_template` SET `minhealth` = '14355', `maxhealth` = '15720' WHERE `entry` = '11450';
+UPDATE `creature_template` SET `minhealth` = '23688', `maxhealth` = '23688' WHERE `entry` = '14323';
+UPDATE `creature_template` SET `minhealth` = '28296', `maxhealth` = '28296' WHERE `entry` = '14325';
+UPDATE `creature_template` SET `minhealth` = '29133', `maxhealth` = '29133' WHERE `entry` = '11501';
+UPDATE `creature_template` SET `minhealth` = '19536', `maxhealth` = '19536' WHERE `entry` = '14324';
+
