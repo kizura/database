c15e3bd837a821795a8eec8d90d39a2620a9eed4
gonzo1247 <gonzo1247@yahoo.de>
2011-09-26 23:52:21 +0200

    DB/Event:  Insert Riggle Bassbait to the creature and game_event.

---
+++ zp_world/custom/26_09_2011_world_creature_game_event_crea_riggle.sql
@@ -0,0 +1,7 @@
+-- ---- creature ----
+
+INSERT INTO `creature` VALUES ('200051', '15077', '0', '0', '0', '-14419.8', '512.409', '4.97048', '5.40627', '25', '5', '0', '3517', '0', '0', '0');
+
+-- ---- game_event_creature ----
+
+INSERT INTO `game_event_creature` VALUES ('200051', '15');
