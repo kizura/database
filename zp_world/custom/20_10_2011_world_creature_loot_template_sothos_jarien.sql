35bf7a1415f34071dfd2645ab0ecf5d4343fa9a3
gonzo1247 <gonzo1247@yahoo.de>
2011-10-25 21:48:11 +0200

    Update loot for sothos and jarien

---
+++ zp_world/custom/20_10_2011_world_creature_loot_template_sothos_jarien.sql
@@ -0,0 +1,13 @@
+-- ---
+-- --- ----- Creature_loot_template -> for npc 16101 (Jarien) adapte the dropp chance for the quest item and insert an item ---------
+
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`,
+`condition_value2`)
+VALUES ('16101', '22329', '0.6', '0', '1', '1', '0', '0', '0');
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = '-44' WHERE `entry` = '16101' AND `item` = '21984';
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = '-54' WHERE `entry` = '16101' AND `item` = '22046;
+
+-- ---- Creature_loot_template -> for npc 16102 (Sothos) adapt the dropp chance for the quest items --------
+
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = '-64' WHERE `entry` = '16102' AND `item` = '21984';
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = '-35' WHERE `entry` = '16102' AND `item` = '22046';
