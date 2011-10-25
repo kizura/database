3ca1acabfdef135f5da89b7f2439e702f45b9385
gonzo1247 <gonzo1247@yahoo.de>
2011-10-25 21:52:19 +0200

    DB/Loot: fix

--- zp_world/custom/20_10_2011_world_creature_loot_template_sothos_jarien.sql
+++ zp_world/custom/20_10_2011_world_creature_loot_template_sothos_jarien.sql
@@ -1,11 +1,11 @@
 -- ---
 -- --- ----- Creature_loot_template -> for npc 16101 (Jarien) adapte the dropp chance for the quest item and insert an item ---------
-
+DELETE FROM `creature_loot_template` WHERE `entry` = '22329' AND `entry` = '16101';
 INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`,
 `condition_value2`)
 VALUES ('16101', '22329', '0.6', '0', '1', '1', '0', '0', '0');
 UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = '-44' WHERE `entry` = '16101' AND `item` = '21984';
-UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = '-54' WHERE `entry` = '16101' AND `item` = '22046;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = '-54' WHERE `entry` = '16101' AND `item` = '22046';
 
 -- ---- Creature_loot_template -> for npc 16102 (Sothos) adapt the dropp chance for the quest items --------
 
