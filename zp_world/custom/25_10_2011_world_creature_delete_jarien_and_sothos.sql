18e90fd6248552210e03120c34d362352974bbb9
gonzo1247 <gonzo1247@yahoo.de>
2011-10-25 22:15:02 +0200

    DB/Bosse: Delete bosses, jarien and sothos.

---
+++ zp_world/custom/25_10_2011_world_creature_delete_jarien_and_sothos.sql
@@ -0,0 +1,7 @@
+-- ---- Despawn von jarien:
+
+DELETE FROM `creature` WHERE `guid` = '151035' AND `id` = '16101';
+
+-- ---- Despawn von sothos:
+
+DELETE FROM `creature` WHERE `guid` = '151036' AND `id` = '16102';
