95b4d833352b084b27b43005acecb60047a6846b
gonzo1247 <gonzo1247@yahoo.de>
2011-10-31 11:28:37 +0100

    DB/Creature: delete kormok

---
+++ zp_world/custom/31_10_2011_world_creature_delete_kormok.sql
@@ -0,0 +1,3 @@
+-- ---- Kormok despawn, sollte erst da sein mit der quest -------------
+
+DELETE FROM `creature` WHERE `guid` = '151037' AND `id` = '16118';
---
+++ zp_world/custom/31_10_2011_world_creature_template_marduk_vectus.sql
@@ -0,0 +1,7 @@
+-- ---- Marduk faction to 35 ---------
+
+UPDATE `creature_template` SET `faction_A` = '35', `faction_H` = '35' WHERE `entry` = '10433';
+
+-- ---- Vectus faction to 35 ---------
+
+UPDATE `creature_template` SET `faction_A` = '35', `faction_H` = '35' WHERE `entry` = '10432';
