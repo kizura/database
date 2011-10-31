d4da0d689616d22368b8e1fd10c27b212cf7f50c
gonzo1247 <gonzo1247@yahoo.de>
2011-10-31 10:56:33 +0100

    DB/Quest: set the minlevel for the quest "Foliant der Ehre" to 20. Fix from oelle

---
+++ zp_world/custom/31_10_2011_world_quest_template_foliant_der_ehre.sql
@@ -0,0 +1,15 @@
+-- ---- Foliant der Ehre, minLevel auf 20 setzen ----------
+-- ---- thx oelle for this fix ---------------------------
+	# foliant der ehre teil 3 mit 20 annehmbar
+ #
+ # Updating object from table 'quest_template'
+ # ID: 1651
+ #
+ UPDATE `quest_template` SET `MinLevel` = '20' WHERE `entry` = '1651';
+ 
+ # teil 4 ebenfalls
+ #
+ # Updating object from table 'quest_template'
+ # ID: 1652
+ #
+ UPDATE `quest_template` SET `MinLevel` = '20' WHERE `entry` = '1652';
