001a8b59055e2d16f3c864dba16b34af95f9e86e
gonzo1247 <gonzo1247@yahoo.de>
2011-11-08 23:03:55 +0100

    DB/Loot: Add loot to npc rend blackhand.

---
+++ zp_world/custom/08_11_2011_world_creature_loot_template_10429.sql
@@ -0,0 +1,32 @@
+-- ------
+
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`,	`condition_value1`, `condition_value2`) 
+VALUES ('10429', '12936', '22', '0', '0', '1', '0', '0', '0');
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`,	`condition_value1`, `condition_value2`) 
+VALUES ('10429', '18103', '21', '0', '0', '1', '0', '0', '0');
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`,	`condition_value1`, `condition_value2`) 
+VALUES ('10429', '18102', '21', '0', '0', '1', '0', '0', '0');
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`,	`condition_value1`, `condition_value2`) 
+VALUES ('10429', '18104', '21', '0', '0', '1', '0', '0', '0');
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`,	`condition_value1`, `condition_value2`) 
+VALUES ('10429', '12935', '21', '0', '0', '1', '0', '0', '0');
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`,	`condition_value1`, `condition_value2`) 
+VALUES ('10429', '16733', '20', '0', '0', '1', '0', '0', '0');
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`,	`condition_value1`, `condition_value2`) 
+VALUES ('10429', '12587', '19', '0', '0', '1', '0', '0', '0');
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`,	`condition_value1`, `condition_value2`) 
+VALUES ('10429', '22247', '17', '0', '0', '1', '0', '0', '0');
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`,	`condition_value1`, `condition_value2`) 
+VALUES ('10429', '12939', '11', '0', '0', '1', '0', '0', '0');
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`,	`condition_value1`, `condition_value2`) 
+VALUES ('10429', '12940', '10', '0', '0', '1', '0', '0', '0');
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`,	`condition_value1`, `condition_value2`) 
+VALUES ('10429', '12583', '9', '0', '0', '1', '0', '0', '0');
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`,	`condition_value1`, `condition_value2`) 
+VALUES ('10429', '12590', '2', '0', '0', '1', '0', '0', '0');
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`,	`condition_value1`, `condition_value2`) 
+VALUES ('10429', '15931', '0.3', '0', '0', '1', '0', '0', '0');
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`,	`condition_value1`, `condition_value2`) 
+VALUES ('10429', '10246', '0.3', '0', '0', '1', '0', '0', '0');
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`,	`condition_value1`, `condition_value2`) 
+VALUES ('10429', '5760', '0.2', '0', '0', '1', '0', '0', '0');
