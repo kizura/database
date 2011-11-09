f45c28c8f513826a001a52a35623e3f463bdef73
gonzo1247 <gonzo1247@yahoo.de>
2011-11-09 02:58:05 +0100

    DB/Loot: This NPC should not have the loot as

---
+++ zp_world/custom/09_11_2011_world_creature_loot_template_rezepte.sql
@@ -0,0 +1,34 @@
+-- ---- rezepte aus der loot db löschen.
+
+-- ---- fuer npc 417 ---------------------
+
+DELETE FROM `creature_loot_template` WHERE `entry` = '417' AND `item` = '18414';
+DELETE FROM `creature_loot_template` WHERE `entry` = '417' AND `item` = '18416';
+DELETE FROM `creature_loot_template` WHERE `entry` = '417' AND `item` = '18418';
+DELETE FROM `creature_loot_template` WHERE `entry` = '417' AND `item` = '18514';
+DELETE FROM `creature_loot_template` WHERE `entry` = '417' AND `item` = '18516';
+DELETE FROM `creature_loot_template` WHERE `entry` = '417' AND `item` = '18517';
+DELETE FROM `creature_loot_template` WHERE `entry` = '417' AND `item` = '18518';
+DELETE FROM `creature_loot_template` WHERE `entry` = '417' AND `item` = '18519';
+
+-- ---- fuer npc 11448 -----------------
+
+DELETE FROM `creature_loot_template` WHERE `entry` = '11448' AND `item` = '18414';
+DELETE FROM `creature_loot_template` WHERE `entry` = '11448' AND `item` = '18416';
+DELETE FROM `creature_loot_template` WHERE `entry` = '11448' AND `item` = '18418';
+DELETE FROM `creature_loot_template` WHERE `entry` = '11448' AND `item` = '18514';
+DELETE FROM `creature_loot_template` WHERE `entry` = '11448' AND `item` = '18516';
+DELETE FROM `creature_loot_template` WHERE `entry` = '11448' AND `item` = '18517';
+DELETE FROM `creature_loot_template` WHERE `entry` = '11448' AND `item` = '18518';
+DELETE FROM `creature_loot_template` WHERE `entry` = '11448' AND `item` = '18519';
+
+-- ---- fuer npc 11450 -----------------
+
+DELETE FROM `creature_loot_template` WHERE `entry` = '11450' AND `item` = '18414';
+DELETE FROM `creature_loot_template` WHERE `entry` = '11450' AND `item` = '18416';
+DELETE FROM `creature_loot_template` WHERE `entry` = '11450' AND `item` = '18418';
+DELETE FROM `creature_loot_template` WHERE `entry` = '11450' AND `item` = '18514';
+DELETE FROM `creature_loot_template` WHERE `entry` = '11450' AND `item` = '18516';
+DELETE FROM `creature_loot_template` WHERE `entry` = '11450' AND `item` = '18517';
+DELETE FROM `creature_loot_template` WHERE `entry` = '11450' AND `item` = '18518';
+DELETE FROM `creature_loot_template` WHERE `entry` = '11450' AND `item` = '18519';
